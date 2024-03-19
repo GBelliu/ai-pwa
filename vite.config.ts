e505adce0501695198174f36ba72cae69c1bf49f
Guilherme <gui.bellissimo@gmail.com>
2024-03-19 01:56:55 -0300

    change project name

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        name: "AI - Obras",
+        short_name: "AI - Obras",
         theme_color: "#282828",
         icons: [
           {
